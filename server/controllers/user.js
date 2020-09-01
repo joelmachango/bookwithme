11389b83e9f8614e12e2546cf6e089d0828cc133
Joel Machango <joelmachango@gmail.com>
2020-09-01 15:39:29 +0300

    Encrypt user password and handle mongoose errors

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,7 @@
+const User = require('../models/user')
+const MongooseHelpers = require('../helpers/mongoose')
+
+
 exports.auth = function (req, res) {
 
 }
@@ -8,8 +12,41 @@ exports.register = function (req, res) {
   // const password = req.body.passowrd;
   // const passwordConfirmation = req.body.passwordConfirmation;
 
-  // Destructurization
+  // Destructurizing
   const { username, email, password, passwordConfirmation } = req.body
 
-  res.json({ username, email })
+  if (!email || !password) {
+    return res.status(422).send({ errors: [{ title: 'Data missing', details: 'Please provide email and password' }] })
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.status(422).send({ errors: [{ title: 'Invalid Password', details: 'Entered password is not the same as confirmation' }] })
+  }
+
+  User.findOne({ email }, function (err, existingUser) {
+    if (err) {
+      return res.status(422).send({ errors: MongooseHelpers.normalizeErrors(err.errors) })
+    }
+
+    if (existingUser) {
+      return res.status(422).send({ errors: [{ title: 'Invalid email', details: 'User with this email already exists' }] })
+    }
+
+    const user = new User({
+      username,
+      email,
+      password
+    })
+
+    user.save(function (err) {
+      if (err) {
+        // return res.status(422).send(err)
+        return res.status(422).send({ errors: MongooseHelpers.normalizeErrors(err.errors) })
+      }
+
+      return res.json({ 'registered': true })
+    })
+  })
+
+  // res.json({ username, email, password, passwordConfirmation })
 }
--- server/models/user.js
+++ server/models/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt')
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -5,12 +6,12 @@ const userSchema = new Schema({
   username: {
     type: String,
     min: [4, 'Too short, minimum of 4 characters'],
-    nax: [32, 'Too long, maximum is 32 characters']
+    max: [32, 'Too long, maximum is 32 characters']
   },
   email: {
     type: String,
     min: [4, 'Too short, minimum of 4 characters'],
-    nax: [32, 'Too long, maximum is 32 characters'],
+    max: [32, 'Too long, maximum is 32 characters'],
     required: 'Email is required',
     lowercase: true,
     unique: true,
@@ -19,10 +20,22 @@ const userSchema = new Schema({
   password: {
     type: String,
     min: [4, 'Too short, minimum of 4 characters'],
-    nax: [32, 'Too long, maximum is 32 characters'],
+    max: [32, 'Too long, maximum is 32 characters'],
     required: 'Password is required'
   },
   rentals: [{ type: Schema.Types.ObjectId, ref: 'Rental' }]
+})
+
+userSchema.pre('save', function (next) {
+  const user = this;
+
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      user.password = hash
+      next()
+    });
+  });
+
 })
 
 module.exports = mongoose.model('User', userSchema)
