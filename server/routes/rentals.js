4f7d7f7a01a6601008483b5c618abc3f67b59a48
Joel Machango <joelmachango@gmail.com>
2020-10-06 13:02:31 +0300

    Add create rental endpoint

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router();
 const Rental = require('../models/rental')
+const User = require('../models/user')
+
 const UserCtrl = require('../controllers/user');
 const { json } = require('body-parser');
 const { normalizeErrors } = require('../helpers/mongoose')
@@ -23,6 +25,21 @@ router.get('/:id', function (req, res) {
     })
 });
 
+router.post('', UserCtrl.authMiddleware, function (req, res) {
+  const { title, city, street, category, image, shared, bedrooms, description, dailyRate } = req.body;
+  const user = res.locals.user
+
+  const rental = new Rental({ title, city, street, category, image, shared, bedrooms, description, dailyRate })
+
+  Rental.create(rental, function (err, newRental) {
+    if (err) {
+      return res.status(422).send({ errors: normalizeErrors(error.errors) });
+    }
+    User.update({ _id: user.id }, { $push: { rentals: newRental } })
+    return res.json(newRental)
+  })
+})
+
 router.get('', function (req, res) {
   const city = req.query.city;
 
@@ -41,7 +58,6 @@ router.get('', function (req, res) {
         }
         return res.json(filteredRentals)
       })
-    return json({ city })
   } else {
     Rental.find({})
       .select('-bookings')
@@ -50,7 +66,5 @@ router.get('', function (req, res) {
       });
   }
 });
-
-
 
 module.exports = router;
