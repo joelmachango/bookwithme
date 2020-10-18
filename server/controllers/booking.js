745e98340d4d605541c0414d8941fa71956382ea
Joel Machango <joelmachango@gmail.com>
2020-10-18 18:19:38 +0300

    Pull booking with rental

--- server/controllers/booking.js
+++ server/controllers/booking.js
@@ -51,7 +51,7 @@ exports.getUserBookings = function (req, res) {
   const user = res.locals.user
   Booking
     .where({ user: user })
-    .populate('rentals')
+    .populate('rental')
     .exec(function (err, foundBookings) {
       if (err) {
         return res.status(422).send({ errors: normalizeErrors(err.errors) })
