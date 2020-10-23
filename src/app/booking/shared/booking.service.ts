39359b7740427ff2c24ca313a51b79929794d65c
Joel Machango <joelmachango@gmail.com>
2020-10-23 05:12:43 +0300

    Connect manage components to server

--- src/app/booking/shared/booking.service.ts
+++ src/app/booking/shared/booking.service.ts
@@ -14,4 +14,8 @@ export class BookingService {
     return this.http.post('api/v1/bookings', booking)
   }
 
+  public getUserBookings(): Observable<any> {
+    return this.http.get('/api/v1/bookings/manage')
+  }
+
 }
